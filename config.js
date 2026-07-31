6cc5dce6a5f32fab722f270c202dbf3887e87919
Sasin195 <donebysasi195@gmail.com>
2026-07-31 12:13:46 +0530

    B-Split donation site

---
+++ config.js
@@ -0,0 +1,9 @@
+// Single config file for the B-Split donation site.
+// Edit these values and the site updates automatically.
+window.SITE_CONFIG = {
+  contactEmail: 'yourname@example.com',
+  upiId: 'sashanknamana@slc',
+  payerName: 'Mr Namana Sashank',
+  // Put your latest app file in the "downloads" folder and set the filename here.
+  downloadUrl: 'downloads/B-Split.apk',
+}
